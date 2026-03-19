cb9905d7a9626de63f8785f0d0655c3ab67364f5
Viniciusdpb <viniciusbeckert.07@gmail.com>
2026-03-19 20:32:55 -0300

    a

--- src/app.js
+++ src/app.js
@@ -1,27 +1,19 @@
 const express = require("express")
-
+const userController = require("./controllers/userController")
 const app = express()
 
 
 app.get("/",(request, response) => {
-  response.send('aaaaaaaaaaaaaaaaaaaaaaaa')
+  response.send('aaaaaaaaaaaaaaaaaaaaaaa')
 
   })
-app.get("/users", (req, res) => {
-  res.send([
-  {
-  "id": 1,
-  "name" : "Joao",
-  "idade" : 25
-  },
-  {
-  "id" : 2,
-  "name" : "Paulo",
-  "idade" : 27
-  }
+app.use(express.json())
 
-  ])
+app.post("/users", (req, res) => {
+    const name = req.body.name
+    res.send("Estamos testando")
 })
-  
+
+  app.get("/users", userController.getAllUsers)
 module.exports = app
 
